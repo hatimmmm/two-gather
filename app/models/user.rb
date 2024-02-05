3ed938186b3f3f79d09056e6fa346a273604bbc9
hatim ourrach <ourrachhatim0@gmail.com>
2024-02-05 17:00:34 +0100

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,22 +16,21 @@ class User < ApplicationRecord
 
   has_one_attached :avatar
 
-  # validates :email, presence: true, uniqueness: true
-  # validates :password, presence: true, length: { minimum: 6 }
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates :bio, length: { maximum: 200 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :bio, length: { maximum: 200 }
 
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def avatar_thumbnail
-    # if avatar.attached?
-    #   avatar.variant(resize: "150x150!")
-    # else
-    #   "default_avatar.jpg"
-    # end
-    
+    if avatar.attached?
+      avatar.variant(resize: "150x150!")
+    else
+      "default_avatar.jpg"
+    end
   end
 end
